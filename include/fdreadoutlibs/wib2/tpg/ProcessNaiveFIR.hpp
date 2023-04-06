a86fb5c0d77adfff2df381fb6c0e411d9a053619
adam-abed-abud <aaadam94@gmail.com>
2023-04-06 15:27:01 +0200

    Fix for tpg threshold not being taken into account

--- include/fdreadoutlibs/wib2/tpg/ProcessNaiveFIR.hpp
+++ include/fdreadoutlibs/wib2/tpg/ProcessNaiveFIR.hpp
@@ -1,8 +1,8 @@
 /**
- * @file ProcessNaive.hpp Non AVX implementation of a simplified hit finding algorithm
- * @author Adam Abed Abud (adam.abed.abud@cern.ch)
+ * @file ProcessNaive.hpp Non AVX implementation of sw tpg
+ * @author Philip Rodrigues (rodriges@fnal.gov)
  *
- * This is part of the DUNE DAQ , copyright 2023.
+ * This is part of the DUNE DAQ , copyright 2020.
  * Licensing/copyright details are in the COPYING file that you should have
  * received with this code.
  */
@@ -42,7 +42,10 @@ template<size_t NREGISTERS>
 void
 process_window_naive(ProcessingInfo<NREGISTERS>& info)
 {
-
+  // Start with taps as floats that add to 1. Multiply by some
+  // power of two (2**N) and round to int. Before filtering, cap the
+  // value of the input to INT16_MAX/(2**N)
+  const size_t NTAPS = 8;
   const int16_t adcMax = info.adcMax;
 
   uint16_t* output_loc = info.output;           // NOLINT
@@ -60,14 +63,21 @@ process_window_naive(ProcessingInfo<NREGISTERS>& info)
     // Get all the state variables by reference so they "automatically" get saved for the next go-round
     ChanState<NREGISTERS>& state = info.chanState;
     int16_t& median = state.pedestals[ichan];
+    int16_t& quantile25 = state.quantile25[ichan];
+    int16_t& quantile75 = state.quantile75[ichan];
     int16_t& accum = state.accum[ichan];
+    int16_t& accum25 = state.accum25[ichan];
+    int16_t& accum75 = state.accum75[ichan];
 
+    // Variables for filtering
+    int16_t* prev_samp = state.prev_samp + NTAPS * ichan;
 
     // Variables for hit finding
     int16_t& prev_was_over = state.prev_was_over[ichan]; // was the previous sample over threshold?
     int16_t& hit_charge = state.hit_charge[ichan];
     int16_t& hit_tover = state.hit_tover[ichan]; // time over threshold
 
+    uint16_t absTimeModNTAPS = info.absTimeModNTAPS; // NOLINT
 
     for (size_t itime = 0; itime < info.timeWindowNumFrames; ++itime) {
       const size_t msg_index = itime / 12;
@@ -82,19 +92,40 @@ process_window_naive(ProcessingInfo<NREGISTERS>& info)
       // --------------------------------------------------------------
       int16_t sample = input16[index];
 
-
+      if (sample < median)
+        frugal_accum_update(quantile25, sample, accum25, 10);
+      if (sample > median)
+        frugal_accum_update(quantile75, sample, accum75, 10);
       frugal_accum_update(median, sample, accum, 10);
 
-      // Subtract the baseline
+      const int16_t sigma = quantile75 - quantile25;
+
       sample -= median;
+
+      // --------------------------------------------------------------
+      // Filtering
+      // --------------------------------------------------------------
+
+      // Don't let the sample exceed adcMax, which is the value
+      // at which its filtered version might overflow
+      sample = std::min(sample, adcMax);
+      int16_t filt_tmp = 0;
+      for (size_t j = 0; j < NTAPS; ++j) {
+        filt_tmp += info.taps[j] * prev_samp[(j + absTimeModNTAPS) % NTAPS];
+      }
+      prev_samp[absTimeModNTAPS % NTAPS] = sample;
+
+      absTimeModNTAPS = (absTimeModNTAPS + 1) % NTAPS;
+      int16_t filt = filt_tmp;
 
       // --------------------------------------------------------------
       // Hit finding
       // --------------------------------------------------------------
-      bool is_over = sample > info.threshold;
+      bool is_over = filt > 5 * sigma * info.multiplier;
       if (is_over) {
         // Simulate saturated add
         int32_t tmp_charge = hit_charge;
+        tmp_charge += filt >> info.tap_exponent;
         tmp_charge = std::min(tmp_charge, (int32_t)std::numeric_limits<int16_t>::max());
         hit_charge = (int16_t)tmp_charge;
         hit_tover++;
@@ -124,6 +155,7 @@ process_window_naive(ProcessingInfo<NREGISTERS>& info)
 
   // printf("Found %d hits\n", nhits);
   info.nhits += nhits;
+  info.absTimeModNTAPS = (info.absTimeModNTAPS + info.timeWindowNumFrames) % NTAPS;
 
   // Write a magic "end-of-hits" value into the list of hits
   for (int i = 0; i < 4; ++i) {
