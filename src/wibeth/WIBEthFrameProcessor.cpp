3f5880865076f1ac18d128d6ff294fc45b0268ec
adam-abed-abud <aaadam94@gmail.com>
2023-06-01 15:54:49 +0200

    Adding WIBEth test expansion code

--- src/wibeth/WIBEthFrameProcessor.cpp
+++ src/wibeth/WIBEthFrameProcessor.cpp
@@ -2,7 +2,7 @@
  * @file WIBEthFrameProcessor.hpp WIBEth specific Task based raw processor
  * @author Adam Abed Abud (adam.abed.abud@cern.ch)
  *
- * This is part of the DUNE DAQ , copyright 2022.
+ * This is part of the DUNE DAQ , copyright 2023.
  * Licensing/copyright details are in the COPYING file that you should have
  * received with this code.
  */
