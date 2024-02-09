c63bf9b230cba6c810f7753a6da462b54f5eb4fa
adam-abed-abud <aaadam94@gmail.com>
2024-02-09 10:28:13 +0100

    Adding Standard Running Sum algorithm. Renamed RS to AbsRS

--- src/wibeth/tpg/ProcessStandardRSAVX2.cpp
+++ src/wibeth/tpg/ProcessStandardRSAVX2.cpp
@@ -1,9 +1,9 @@
 /**
- * @file ProcessAVX2.cpp
+ * @file ProcessStandardRSAVX2.cpp
  *
  * This is part of the DUNE DAQ , copyright 2020.
  * Licensing/copyright details are in the COPYING file that you should have
  * received with this code.
  */
 
-#include "fdreadoutlibs/wibeth/tpg/ProcessRSAVX2.hpp"
+#include "fdreadoutlibs/wibeth/tpg/ProcessStandardRSAVX2.hpp"
