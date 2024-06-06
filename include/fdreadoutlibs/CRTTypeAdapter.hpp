c0570db6461db826fa3ae54412e43748f99484f1
np04daq <np04daq@np04-srv-024.cern.ch>
2024-06-06 09:28:57 +0200

    updating CRTTypeAdapter to be more like the TriggerPrimitiveTypeAdapter

--- include/fdreadoutlibs/CRTTypeAdapter.hpp
+++ include/fdreadoutlibs/CRTTypeAdapter.hpp
@@ -22,47 +22,73 @@ namespace types {
 const constexpr std::size_t kCRTSize = 288; 
 struct CRTTypeAdapter
 {
-  using FrameType = dunedaq::fddetdataformats::CRTFrame;
+  using FrameType = CRTTypeAdapter;
   // data
-  char data[kCRTSize];
+  //char data[kCRTSize];
+  dunedaq::fddetdataformats::CRTFrame crtdata;
   // comparable based on first timestamp
   bool operator<(const CRTTypeAdapter& other) const
   {
-    auto thisptr = reinterpret_cast<const FrameType*>(&data);        // NOLINT
-    auto otherptr = reinterpret_cast<const FrameType*>(&other.data); // NOLINT
-    return thisptr->get_timestamp() < otherptr->get_timestamp() ? true : false;
+    //auto thisptr = reinterpret_cast<const FrameType*>(&data);        // NOLINT
+    //auto otherptr = reinterpret_cast<const FrameType*>(&other.data); // NOLINT
+
+    //if(thisptr->get_timestamp()==otherptr->get_timestamp())
+    //return thisptr->get_module() < otherptr->get_module() ? true : false;
+    //return thisptr->get_timestamp() < otherptr->get_timestamp() ? true : false;
+
+    if(this->crtdata.get_timestamp()==other.crtdata.get_timestamp())
+      return this->crtdata.get_module() < other.crtdata.get_module() ? true : false;
+    return this->crtdata.get_timestamp() < other.crtdata.get_timestamp() ? true : false;
+
+  }
+
+  uint64_t get_timestamp() const // NOLINT(build/unsigned)
+  {
+    return crtdata.get_timestamp();
   }
 
   uint64_t get_first_timestamp() const // NOLINT(build/unsigned)
   {
-    return reinterpret_cast<const FrameType*>(&data)->get_timestamp(); // NOLINT
+    //return reinterpret_cast<const FrameType*>(&data)->get_timestamp(); // NOLINT
+    return crtdata.get_timestamp();
   }
 
   void set_first_timestamp(uint64_t ts) // NOLINT(build/unsigned)
   {
-    auto frame = reinterpret_cast<FrameType*>(&data); // NOLINT
-    frame->set_timestamp(ts);
+    //auto frame = reinterpret_cast<FrameType*>(&data); // NOLINT
+    //frame->set_timestamp(ts);
+
+    crtdata.set_timestamp(ts);
   }
 
   void fake_timestamps(uint64_t first_timestamp, uint64_t /*offset = 2048*/ ) // NOLINT(build/unsigned)
   {
-    auto wef = reinterpret_cast<FrameType*>(((uint8_t*)(&data))); // NOLINT
-    wef->set_timestamp(first_timestamp);
+    // auto wef = reinterpret_cast<FrameType*>(((uint8_t*)(&data))); // NOLINT
+    //wef->set_timestamp(first_timestamp);
+
+    crtdata.set_timestamp(first_timestamp);
+    
   }
 
   void fake_geoid(uint16_t crate_id, uint16_t slot_id, uint16_t stream_id) {
-      for (unsigned int i = 0; i < get_num_frames(); ++i) {
-        auto df = reinterpret_cast<FrameType*>((reinterpret_cast<uint8_t*>(&data)) + i * get_frame_size());
+    /*
+    for (unsigned int i = 0; i < get_num_frames(); ++i) {
+        auto df = reinterpret_cast<FrameType*>((reinterpret_cast<uint8_t*>(&crtdata)) + i * get_frame_size());
         df->daq_header.crate_id = crate_id;
         df->daq_header.slot_id = slot_id;
         df->daq_header.stream_id = stream_id;
       }
+    */
+    crtdata.daq_header.crate_id = crate_id;
+    crtdata.daq_header.slot_id = slot_id;
+    crtdata.daq_header.stream_id = stream_id;
   }
 
   void fake_adc_pattern(int channel) {
-    auto frame = reinterpret_cast<FrameType*>(&data); // NOLINT
+    //auto frame = reinterpret_cast<FrameType*>(&data); // NOLINT
     // Set the ADC to the int16 maximum value 
-    frame->set_adc(channel, 8191); 
+    //frame->set_adc(channel, 8191);
+    crtdata.set_adc(channel,8191);
   }
 
   void fake_frame_errors(std::vector<uint16_t>* /*fake_errors*/) // NOLINT
@@ -72,12 +98,14 @@ struct CRTTypeAdapter
 
   FrameType* begin()
   {
-    return reinterpret_cast<FrameType*>(&data[0]); // NOLINT
+    //return reinterpret_cast<FrameType*>(&data[0]); // NOLINT
+    return this;
   }
 
   FrameType* end()
   {
-    return reinterpret_cast<FrameType*>(data + kCRTSize); // NOLINT
+    //return reinterpret_cast<FrameType*>(&data[0] + kCRTSize); // NOLINT
+    return (this + 1);
   }
 
   size_t get_payload_size() { return kCRTSize; }
@@ -89,7 +117,7 @@ struct CRTTypeAdapter
   static const constexpr size_t fixed_payload_size = 288;
   static const constexpr daqdataformats::SourceID::Subsystem subsystem = daqdataformats::SourceID::Subsystem::kDetectorReadout;
   static const constexpr daqdataformats::FragmentType fragment_type = daqdataformats::FragmentType::kCRT;
-  static const constexpr uint64_t expected_tick_difference = 16; // NOLINT(build/unsigned)
+  static const constexpr uint64_t expected_tick_difference = 1; // NOLINT(build/unsigned)
 };
 
 static_assert(sizeof(struct dunedaq::fddetdataformats::CRTFrame) == kCRTSize,
