55d2de3ae6c2497cf1cf928e66a9fecfcf647c72
Wesley Ketchum <wesketchum@gmail.com>
2024-10-28 11:40:04 +0100

    update for request handler test to now work on buffers with skipped elements

--- unittest/FDReadoutRequestHandlers_test.cxx
+++ unittest/FDReadoutRequestHandlers_test.cxx
@@ -100,10 +100,10 @@ void test_request_model()
   uint64_t ticks_between = ticks_per_frame*n_frames;
 
 
-  //
+  // function for testing get_fragment_pieces in cases where there are no skips in the buffer
   auto test_req_bounds = [&](std::shared_ptr<BufferType<TypeAdapter>> buffer,
                              uint64_t start_win, uint64_t end_win,
-                             bool check_exact){
+                             std::set<size_t> objects_skipped={}){
 
       //make the error registry we need
       auto errorRegistry = std::make_unique<dunedaq::datahandlinglibs::FrameErrorRegistry>();
@@ -120,8 +120,8 @@ void test_request_model()
       BOOST_CHECK_EQUAL(req_res.result_code,DefaultRequestHandler::ResultCode::kFound);
 
       //check that the first and last returned blocks are note empty
-      BOOST_CHECK_GT(ret.front().second,0);
-      BOOST_CHECK_GT(ret.back().second,0);
+      BOOST_REQUIRE_GT(ret.front().second,0);
+      BOOST_REQUIRE_GT(ret.back().second,0);
 
       //grab the (first) timestamps of the first and last frames
       uint64_t first_ts = reinterpret_cast<const TypeAdapter::FrameType*>(ret.front().first)->get_timestamp();
@@ -130,26 +130,42 @@ void test_request_model()
       //general check:
       // first_ts <= start_win < first_ts + ticks_per_frame
       // last_ts < end_win <= last_ts + ticks_per_frame
-      BOOST_CHECK_MESSAGE((first_ts<=start_win && start_win<(first_ts+TypeAdapter::expected_tick_difference)),
-                          "Check first_frame_ts{" << first_ts << "} <= start_win{"
-                          << start_win << "} < first_frame_ts+ticks_per_frame{" << first_ts+TypeAdapter::expected_tick_difference << "}");
-      BOOST_CHECK_MESSAGE((last_ts<end_win && end_win<=(last_ts+TypeAdapter::expected_tick_difference)),
-                          "Check last_frame_ts{" << last_ts << "} < end_win{"
-                          << end_win << "} < last_frame_ts+ticks_per_frame{" << last_ts+TypeAdapter::expected_tick_difference << "}");
+      if(objects_skipped.size()==0)
+          BOOST_CHECK_MESSAGE(first_ts<=start_win,
+                              "first_frame_ts{" << first_ts << "} <= start_win{" << start_win << "}");
+      BOOST_CHECK_MESSAGE(start_win<first_ts+ticks_per_frame,
+                          "start_win{" << start_win << "} < first_frame_ts+ticks_per_frame{" << first_ts+ticks_per_frame << "}");
+      BOOST_CHECK_MESSAGE(last_ts<end_win,
+                          "Check last_frame_ts{" << last_ts << "} < end_win{" << end_win << "}");
+      if(objects_skipped.size()==0)
+          BOOST_CHECK_MESSAGE(end_win<=last_ts+ticks_per_frame,
+                              "end_win{" << end_win << "} <= last_frame_ts+ticks_per_frame{" << last_ts+ticks_per_frame << "}");
 
-      if(check_exact) {
-          auto expected_start = TypeAdapter::expected_tick_difference *
-                                (uint64_t) std::floor((float) (start_win) / (float) (ticks_per_frame));
-          auto expected_end = TypeAdapter::expected_tick_difference *
-                              (uint64_t) std::ceil((float) (end_win) / (float) (ticks_per_frame));
+      //expected timestamps for begin of fragment and 'end' of fragment, assuming no skipping
+      auto expected_start = TypeAdapter::expected_tick_difference *
+              (uint64_t) std::floor((float) (start_win) / (float) (ticks_per_frame));
+      auto expected_end = TypeAdapter::expected_tick_difference *
+              (uint64_t) std::ceil((float) (end_win) / (float) (ticks_per_frame));
 
-          //specfic check: are values for this request what we expect
-          BOOST_CHECK_MESSAGE(first_ts == expected_start,
-                              "Fragment start ts {" << first_ts << "} is expected value {" << expected_start << "}");
-          BOOST_CHECK_MESSAGE((last_ts + TypeAdapter::expected_tick_difference) == expected_end,
-                              "Fragment 'end' ts {" << last_ts + TypeAdapter::expected_tick_difference
-                                                    << "} is expected value {" << expected_end << "}");
+      //correct for the cases there that object has been skipped
+      auto expected_start_obj = expected_start - expected_start%ticks_between;
+      while(objects_skipped.count(expected_start_obj/ticks_between)>0) {
+          expected_start += ticks_per_frame;
+          expected_start_obj = expected_start - expected_start%ticks_between;
       }
+
+      auto expected_end_obj = expected_end - ticks_per_frame - (expected_end-ticks_per_frame)%ticks_between;
+      while(objects_skipped.count(expected_end_obj/ticks_between)>0) {
+          expected_end -= ticks_per_frame;
+          expected_start_obj = expected_end - ticks_per_frame - (expected_end-ticks_per_frame)%ticks_between;
+      }
+
+      //specfic check: are values for this request what we expect
+      BOOST_CHECK_MESSAGE(first_ts == expected_start,
+                          "Fragment start ts {" << first_ts << "} is expected value {" << expected_start << "}");
+      BOOST_CHECK_MESSAGE((last_ts + TypeAdapter::expected_tick_difference) == expected_end,
+                          "Fragment 'end' ts {" << last_ts + TypeAdapter::expected_tick_difference
+                          << "} is expected value {" << expected_end << "}");
   };
 
   /*
@@ -162,26 +178,28 @@ void test_request_model()
   fill_buffer<BufferType,TypeAdapter>(buffer_noskip,0,10);
   print_buffer<BufferType,TypeAdapter>(buffer_noskip,"noskip");
 
-  test_req_bounds(buffer_noskip,ticks_between*2,ticks_between*5,true);
-  test_req_bounds(buffer_noskip,ticks_between*3/2,ticks_between*9/2,true);
-  test_req_bounds(buffer_noskip,ticks_between*2+1,ticks_between*5+1,true);
+  test_req_bounds(buffer_noskip,ticks_between*2,ticks_between*5);
+  test_req_bounds(buffer_noskip,ticks_between*3/2,ticks_between*9/2);
+  test_req_bounds(buffer_noskip,ticks_between*11/5,ticks_between*21/5);
+  test_req_bounds(buffer_noskip,ticks_between*2+1,ticks_between*5+1);
 
   /*
    * Skipped buffer should have elements with index [0, 1 , 2 , 3 , 4 , 5 , 6 , 7 ,  8 ,  9 ]
    *                                 and timestamps [0,1*T,2*T,5*T,6*T,7*T,8*T,9*T,10*T,11*T]
    * where T = DTS ticks between successive elements (tick_diff_per_frame * n_frames_per_obj_in_buffer)
   */
-  /*
+
   BOOST_TEST_MESSAGE("Testing buffer with skips...");
   std::set<size_t> obj_to_skip = {2,3};
   auto buffer_skip = std::make_shared< BufferType<TypeAdapter> >();
   fill_buffer<BufferType,TypeAdapter>(buffer_skip,0,10,obj_to_skip);
   print_buffer<BufferType,TypeAdapter>(buffer_skip,"skip");
 
-    test_req_bounds(ticks_between*2,ticks_between*5,false);
-    test_req_bounds(ticks_between*3/2,ticks_between*9/2,false);
-    test_req_bounds(ticks_between*2+1,ticks_between*5+1,false);
-*/
+  test_req_bounds(buffer_skip,ticks_between*2,ticks_between*5,obj_to_skip);
+  test_req_bounds(buffer_skip,ticks_between*3/2,ticks_between*9/2,obj_to_skip);
+  test_req_bounds(buffer_skip,ticks_between*11/5,ticks_between*21/5,obj_to_skip);
+  test_req_bounds(buffer_skip,ticks_between*2+1,ticks_between*5+1,obj_to_skip);
+
 }
 
 BOOST_AUTO_TEST_SUITE(FDReadoutRequestHandlers_test)
