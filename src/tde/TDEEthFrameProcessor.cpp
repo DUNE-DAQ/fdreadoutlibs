959061f27ff303ee9cd28996a0a1a8509292cd76
Deniz Tuana Ergonul Uzun <deniz.tuana.ergonul@cern.ch>
2024-10-31 11:02:01 +0100

    Ignore TDEEthFrameProcessor for now

--- src/tde/TDEEthFrameProcessor.cpp
+++ src/tde/TDEEthFrameProcessor.cpp
@@ -42,7 +42,6 @@ using dunedaq::datahandlinglibs::logging::TLVL_TAKE_NOTE;
 
 // THIS SHOULDN'T BE HERE!!!!! But it is necessary.....
 DUNE_DAQ_TYPESTRING(dunedaq::trigger::TriggerPrimitiveTypeAdapter, "TriggerPrimitive")
-DUNE_DAQ_TYPESTRING(dunedaq::trigger::TriggerPrimitiveTypeAdapter::TPAArrayPair, "TriggerPrimitiveVector")
 
 
 namespace dunedaq {
