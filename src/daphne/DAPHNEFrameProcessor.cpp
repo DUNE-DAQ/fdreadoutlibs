4f52413c0213471d55221124432046c080ed9410
Robert Mihai Amarinei <robert.amarinei@icloud.com>
2025-06-17 18:09:21 +0200

    det_id, slot_id, link_id retrieved from DAQHeader

--- src/daphne/DAPHNEFrameProcessor.cpp
+++ src/daphne/DAPHNEFrameProcessor.cpp
@@ -6,11 +6,12 @@
  * Licensing/copyright details are in the COPYING file that you should have
  * received with this code.
  */
+
 #include "fddetdataformats/DAPHNEFrame.hpp"
 #include "trgdataformats/TriggerPrimitive.hpp"
 #include "fdreadoutlibs/daphne/DAPHNEFrameProcessor.hpp"
 
- #include "confmodel/GeoId.hpp"
+#include "confmodel/GeoId.hpp"
 
 #include <atomic>
 #include <functional>
@@ -52,7 +53,7 @@ DAPHNEFrameProcessor::conf(const appmodel::DataHandlerModule* conf)
   auto proc_conf = dp->cast<appmodel::PDSRawDataProcessor>();
   m_def_adc_intg_thresh = proc_conf-> get_default_adc_intg_thresh();
 
-  TLOG() << "RMA 1"; 
+ 
   auto geo_id = conf->get_geo_id();
   if (geo_id != nullptr) {
     m_det_id = geo_id->get_detector_id();
@@ -61,10 +62,10 @@ DAPHNEFrameProcessor::conf(const appmodel::DataHandlerModule* conf)
     m_stream_id = geo_id->get_stream_id();
   }
 
-  TLOG() << "RMA 2";
+
   m_channel_map = dunedaq::detchannelmaps::make_pds_map(proc_conf->get_channel_map());
   const std::vector<unsigned int> channel_mask_vec = proc_conf->get_channel_mask();
-  TLOG() << "RMA 3";
+
   for (int chan = 0; chan < 48; chan++) {// 40 physical PDS channel 8 not. 0->7 contain light info, 8,9, additional info. 10-17 light, 18,19 not etc...  
     trgdataformats::channel_t off_channel = m_channel_map->get_offline_channel_from_det_crate_slot_stream_chan(m_det_id, m_crate_id, m_slot_id, m_stream_id, chan);
     if (std::find(channel_mask_vec.begin(), channel_mask_vec.end(), off_channel) != channel_mask_vec.end())
@@ -74,12 +75,12 @@ DAPHNEFrameProcessor::conf(const appmodel::DataHandlerModule* conf)
 
   TLOG() << "Registering processing tasks...";
   inherited::add_preprocess_task(std::bind(&DAPHNEFrameProcessor::timestamp_check, this, std::placeholders::_1));
-  TLOG() << "RMA 4";  
+
   if (m_post_processing_enabled) { 
     // Extract TPs back as a pre-processing task, due to LatencyBuffer post-proc issues using SkipList.
     inherited::add_preprocess_task(std::bind(&DAPHNEFrameProcessor::extract_tps, this, std::placeholders::_1));
   }
-  TLOG() << "RMA 5";
+
   TLOG() << "Calling parent conf.";
   inherited::conf(conf);
 }
@@ -168,7 +169,7 @@ DAPHNEFrameProcessor::frame_error_check(frameptr /*fp*/)
 
 void DAPHNEFrameProcessor::extract_tps(constframeptr fp)
 {
-  TLOG() << "RMA extract tps";
+
   //  size_t nhits = 0;
   if (!fp || fp==nullptr){
     return;
@@ -185,12 +186,15 @@ void DAPHNEFrameProcessor::extract_tps(constframeptr fp)
     {
       if(df_ptr[i].peaks_data.is_found(j))
       {
-
-        int ch =  m_channel_map->get_offline_channel_from_det_crate_slot_stream_chan(m_det_id,m_crate_id, df_ptr[i].daq_header.slot_id, m_stream_id, df_ptr[i].get_channel());
+        //PUT THE Crate ID...   
+        int ch =  m_channel_map->get_offline_channel_from_det_crate_slot_stream_chan(df_ptr[i].daq_header.det_id, df_ptr[i].daq_header.crate_id, df_ptr[i].daq_header.slot_id, df_ptr[i].daq_header.link_id, df_ptr[i].get_channel());
         // if (is_masked(ch)) continue;
         //the following is not really efficient. A proper masking should be done. 
+
         if (std::binary_search(m_channel_mask_set.begin(), m_channel_mask_set.end(), ch)) continue;
         if (df_ptr[i].peaks_data.get_adc_integral(j) < m_def_adc_intg_thresh) continue;
+
+
         trigger::TriggerPrimitiveTypeAdapter tpa;
         tpa.tp = peak_to_tp(df_ptr[i],j);// this is the trigger primitive
         //check for timestamps that are due to frame timestamps ~ ts=0, and ignore these peaks
@@ -235,7 +239,7 @@ DAPHNEFrameProcessor::peak_to_tp(dunedaq::fddetdataformats::DAPHNEFrame &frame, 
   tp.samples_over_threshold = frame.peaks_data.get_samples_over_baseline(i);
   // FIXME : hard-coded channel map
   // WARNING: slot ids in DAPHNEs are all 0!
-  tp.channel = m_channel_map->get_offline_channel_from_det_crate_slot_stream_chan(m_det_id,m_crate_id, frame.daq_header.slot_id, m_stream_id, frame.get_channel());
+  tp.channel = m_channel_map->get_offline_channel_from_det_crate_slot_stream_chan(frame.daq_header.det_id, frame.daq_header.crate_id, frame.daq_header.slot_id, frame.daq_header.link_id, frame.get_channel());
   tp.adc_integral = frame.peaks_data.get_adc_integral(i);
   tp.adc_peak = frame.peaks_data.get_adc_max(i);
   tp.detid = dunedaq::trgdataformats::INVALID_DETID;
