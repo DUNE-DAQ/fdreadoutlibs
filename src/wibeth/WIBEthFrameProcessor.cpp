64a43b12a2dcce79c50fe621936d75af7884b04b
Theo Yu <xyz.yu@mail.utoronto.ca>
2025-08-04 16:11:45 +0200

    Finish periodic collection and publication of opmon data.

--- src/wibeth/WIBEthFrameProcessor.cpp
+++ src/wibeth/WIBEthFrameProcessor.cpp
@@ -217,8 +217,21 @@ WIBEthFrameProcessor::generate_opmon_data()
      }
      m_t0 = now;
 
-     if (m_tpg_metric_collect_enabled && m_update_metric_opmon.load(std::memory_order_release)) {
+     if (m_tpg_metric_collect_enabled) {
        auto metrics = m_tp_generator->get_processor_metrics();
+
+       for (const auto& [channel, vec] : metrics) {
+        datahandlinglibs::opmon::TPGProcessorInfo tpg_proc_info;
+        for (const auto& [name, val] : vec) {
+          if (name == "m_pedestal") {
+            tpg_proc_info.set_pedestal(val);
+          } else if (name == "m_accum")
+          {
+            tpg_proc_info.set_accum(val);
+          }
+        }
+        publish(std::move(tpg_proc_info), {{"channel", std::to_string(channel)}});
+      }
      }
 
    }
@@ -373,7 +386,8 @@ WIBEthFrameProcessor::find_hits(constframeptr fp)
 
   std::vector<trgdataformats::TriggerPrimitive> tps = (*m_tp_generator)(wfptr);
   m_frame_counter++;
-  if (m_tpg_metric_collect_enabled && m_frame_counter % m_metric_collect_opmon_rate == 0) {
+  m_frame_counter_for_metrics++;
+  if (m_tpg_metric_collect_enabled && m_frame_counter_for_metrics % m_metric_collect_opmon_rate == 0) {
     m_tp_generator->signal_metric_collection();
     m_update_metric_opmon.store(true, std::memory_order_release);
   }
