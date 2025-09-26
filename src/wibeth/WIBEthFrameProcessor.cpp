50ea97967e7c7dbf54e7af0cf61ab4fc57a7745a
ramarine <robert.amarinei@icloud.com>
2025-09-26 17:03:19 +0200

    frame counter incremented after discarding masked channels

--- src/wibeth/WIBEthFrameProcessor.cpp
+++ src/wibeth/WIBEthFrameProcessor.cpp
@@ -484,7 +484,6 @@ WIBEthFrameProcessor::find_hits(constframeptr fp)
   }
 
   std::vector<trgdataformats::TriggerPrimitive> tps = (*m_tp_generator)(wfptr);
-  m_current_tp_count += tps.size();
   m_current_frame_count++;
   if (m_tpg_metric_collect_enabled && m_frame_counter.load(std::memory_order_relaxed) % m_metric_collect_opmon_period == 0) {
     m_tp_generator->signal_metric_collection();
@@ -494,6 +493,7 @@ WIBEthFrameProcessor::find_hits(constframeptr fp)
     // If this TP is on a masked channel, skip it.
     if (std::binary_search(m_channel_mask_set.begin(), m_channel_mask_set.end(), tp.channel))
       continue;
+    m_current_tp_count++;
     // Need to move into a type adapter.
     trigger::TriggerPrimitiveTypeAdapter tpa;
     tpa.tp = tp;
@@ -502,7 +502,8 @@ WIBEthFrameProcessor::find_hits(constframeptr fp)
     m_tp_channel_rate_map[tp.channel]++;
   }
 
-  if (m_current_frame_count > m_frame_count_limit || m_current_tp_count > m_tp_count_limit) {
+  // if (m_current_frame_count > m_frame_count_limit || m_current_tp_count > m_tp_count_limit) {
+  if (m_current_frame_count > m_frame_count_limit == 0 || m_current_tp_count > m_tp_count_limit) {
     for (int i = 0; i < 3; i++) {
       int new_tps = m_tpa_vectors[i].size();
       if (new_tps == 0) {
